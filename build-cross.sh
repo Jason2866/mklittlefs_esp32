8471b7bcb2d01a94659600457e75373b0d30d9ad
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-09-23 16:53:10 +0200

    Platformio package.json

--- build-cross.sh
+++ build-cross.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 name=mklittlefs-$(git rev-parse --short HEAD)
-rel=${rel:=-2.5.0}
-subrel=${subrel:-2}
+rel=${rel:=030100}
 
 rm -f *.json *.gz *.zip
 
 build ()
 {(
-    TARGET_OS=${tgt} CC=${pfx}-gcc CXX=${pfx}-g++ STRIP=${pfx}-strip make clean mklittlefs${exe} BUILD_CONFIG_NAME="-arduino-esp8266" CPPFLAGS="-DSPIFFS_USE_MAGIC_LENGTH=0 -DSPIFFS_ALIGNED_OBJECT_INDEX_TABLES=1"
+    TARGET_OS=${tgt} CC=${pfx}-gcc CXX=${pfx}-g++ STRIP=${pfx}-strip make clean mklittlefs${exe} BUILD_CONFIG_NAME="-arduino-esp32" CPPFLAGS="-DSPIFFS_OBJ_META_LEN=4"
     rm -rf tmp
     mkdir -p tmp/mklittlefs
     mv mklittlefs${exe} tmp/mklittlefs/.
@@ -21,20 +20,17 @@ build ()
     fi
     cd ..
     rm -rf tmp
-    tarballsize=$(stat -c%s ${tarball})
-    tarballsha256=$(sha256sum ${tarball} | cut -f1 -d" ")
     ( echo '            {' &&
-      echo '              "host": "'$AHOST'",' &&
-      echo '              "url": "https://github.com/earlephilhower/mklittlefs/releases/download/'${rel}-${subrel}'/'${tarball}'",' &&
-      echo '              "archiveFileName": "'${tarball}'",' &&
-      echo '              "checksum": "SHA-256:'${tarballsha256}'",' &&
-      echo '              "size": "'${tarballsize}'"' &&
+      echo '              "description": "mklittlefs32",' &&
+      echo '              "version": "2.'${rel}'",' &&
+      echo '              "system": "'$AHOST'",' &&
+      echo '              "url": "https://github.com/Jason2866/mklittlefs/releases/download/'${rel}'/'${tarball}'",' &&
       echo '            }') > ${tarball}.json
 )}
 
-tgt=osx pfx=x86_64-apple-darwin14 exe="" AHOST="x86_64-apple-darwin" build
-tgt=windows pfx=x86_64-w64-mingw32 exe=".exe" AHOST="x86_64-mingw32" build
-tgt=windows pfx=i686-w64-mingw32 exe=".exe" AHOST="i686-mingw32" build
-tgt=linux pfx=arm-linux-gnueabihf exe="" AHOST="arm-linux-gnueabihf" build
-tgt=linux pfx=aarch64-linux-gnu exe="" AHOST="aarch64-linux-gnu" build
-tgt=linux pfx=x86_64-linux-gnu exe="" AHOST="x86_64-pc-linux-gnu" build
+tgt=osx pfx=x86_64-apple-darwin14 exe="" AHOST="['darwin_x86_64','darwin_arm64']" build
+tgt=windows pfx=x86_64-w64-mingw32 exe=".exe" AHOST="windows_amd64" build
+tgt=windows pfx=i686-w64-mingw32 exe=".exe" AHOST="windows_x86" build
+tgt=linux pfx=arm-linux-gnueabihf exe="" AHOST="linux_armv6l" build
+tgt=linux pfx=aarch64-linux-gnu exe="" AHOST="linux_aarch64" build
+tgt=linux pfx=x86_64-linux-gnu exe="" AHOST="linux_x86_64" build
