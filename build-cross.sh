a7e5307c4dcb91878b0a763d56a17ee81723b896
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-09-24 14:58:46 +0200

    Pio package names

--- build-cross.sh
+++ build-cross.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-name=mklittlefs-$(git rev-parse --short HEAD)
 rel=${rel:=4.0.0}
 
 rm -f *.json *.gz *.zip
@@ -7,6 +6,8 @@ rm -f *.json *.gz *.zip
 build ()
 {(
     TARGET_OS=${tgt} CC=${pfx}-gcc CXX=${pfx}-g++ STRIP=${pfx}-strip make clean mklittlefs${exe} BUILD_CONFIG_NAME="-arduino-esp32" CPPFLAGS="-DSPIFFS_OBJ_META_LEN=4"
+    name=$PNAME-${rel}
+    echo $name
     rm -rf tmp
     mkdir -p tmp/mklittlefs
     mv mklittlefs${exe} tmp/mklittlefs/.
@@ -33,9 +34,9 @@ build ()
     cd ..
 )}
 
-tgt=osx pfx=x86_64-apple-darwin14 exe="" AHOST='["darwin_x86_64","darwin_arm64"]' build
-tgt=windows pfx=x86_64-w64-mingw32 exe=".exe" AHOST='["windows_amd64"]' build
-tgt=windows pfx=i686-w64-mingw32 exe=".exe" AHOST='["windows_x86"]' build
-tgt=linux pfx=arm-linux-gnueabihf exe="" AHOST='["linux_armv6l"]' build
-tgt=linux pfx=aarch64-linux-gnu exe="" AHOST='["linux_aarch64"]' build
-tgt=linux pfx=x86_64-linux-gnu exe="" AHOST='["linux_x86_64"]' build
+tgt=osx pfx=x86_64-apple-darwin14 exe="" AHOST='["darwin_x86_64","darwin_arm64"]' PNAME='["tool-mklittlefs-darwin_x86_64"]' build
+tgt=windows pfx=x86_64-w64-mingw32 exe=".exe" AHOST='["windows_amd64"]' PNAME='["tool-mklittlefs-windows_amd64"]' build
+tgt=windows pfx=i686-w64-mingw32 exe=".exe" AHOST='["windows_x86"]' PNAME='["tool-mklittlefs-windows_x86"]' build
+tgt=linux pfx=arm-linux-gnueabihf exe="" AHOST='["linux_armv6l"]' PNAME='["tool-mklittlefs-linux_armv6l"]' build
+tgt=linux pfx=aarch64-linux-gnu exe="" AHOST='["linux_aarch64"]' PNAME='["tool-mklittlefs-linux_aarch64"]' build
+tgt=linux pfx=x86_64-linux-gnu exe="" AHOST='["linux_x86_64"]' PNAME='["tool-mklittlefs-linux_x86_64"]' build
